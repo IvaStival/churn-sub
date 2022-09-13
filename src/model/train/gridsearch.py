695a3961683fded07401b2ae3a2b7ce7fed60bc7
iva stival <ivastival@gmail.com>
2022-09-13 10:24:33 -0300

    Add OOT base and use gridsearch to look for the bests params

--- src/model/train/gridsearch.py
+++ src/model/train/gridsearch.py
@@ -10,11 +10,12 @@ from sklearn import pipeline
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
 
 import scikitplot as sklplot
 
 # %%
-
+## SAMPLE -------------------------------------------------------------------------------------
 ## LOADING DATA BASE TO DF
 def load_data_base(path, database):
     engine = sql.create_engine(f"sqlite:///{path}/{database}.db")
@@ -26,18 +27,27 @@ con = load_data_base(db_path, db_name)
 
 df = pd.read_sql("abt_model_churn", con)
 
+# GET OOT
+df_oot = df.loc[df['dtRef'] == '2022-01-11']
+
+# GET ALL OTHERS
+df_train = df.loc[df['dtRef'] < '2022-01-11']
+
 # %%
 ## REMOVE UNUSED FEATURES
-columns = df.columns
+columns = df_train.columns
 target = "flNaoChurn"
 ids = ["dtRef", "idPlayer", "dtRef:1"]
 to_remove = ["flAssinatura"]
 
 features = list(set(columns) - set([target]) - set(ids) - set(to_remove))
+
 # %%
+
+
 ## SPLIT DATA BETEWEEN TRAN AND TEST
-X_train, X_test, Y_train, Y_test = model_selection.train_test_split(df[features],
-                                                                    df[target],
+X_train, X_test, Y_train, Y_test = model_selection.train_test_split(df_train[features],
+                                                                    df_train[target],
                                                                     test_size=0.2,
                                                                     random_state=42 )
 
@@ -46,7 +56,7 @@ missing_columns = X_train.count()[X_train.count() < X_train.shape[0]].index.toli
 missing_columns.sort()
 missing_columns
  # %% 
-
+## MODEL -------------------------------------------------------------------------------------
 ## VARIABLES
 missing_flags = ["avg1Kill", "avg2Kill", "avg3Kill", "avg4Kill", "avg5Kill", "avgAssist", "avgBombeDefuse", "avgBombePlant", "avgClutchWon", "avgDamage", "avgDeath", "avgFirstKill", "avgFlashAssist", "avgHits", "avgHs", "avgHsRate", "avgKDA", "avgKDR", "avgKill", "avgLastAlive", "avgPlusKill", "avgRoundsPlayed", "avgShots", "avgSurvived", "avgTk", "avgTkAssist", "avgTrade", "qtRecencia","vlHsRate","vlKDA","vlKDR","winRate"]
 missing_zeros = ["propAncient","propDia1","propDia2","propDia3","propDia4","propDia5","propDia6","propDia7","propDust2","propInferno","propMirage","propNuke","propOverpass","propTrain","propVertigo","qtDias","qtPartidas", 'qtExpBatalha', 'qtMedalha', 'qtMedalhaDist', 'qtMedalhaTribo']
@@ -73,6 +83,7 @@ model_pipeline = pipeline.Pipeline([ ("Missing Flag", fe_missing_flags),
 
 model_pipeline.fit(X_train, Y_train)
 # %%
+## PREDICTION -------------------------------------------------------------------------------------
 y_train_pred = model_pipeline.predict(X_train)
 y_test_pred = model_pipeline.predict(X_test)
 
@@ -115,8 +126,18 @@ features_importance = pd.Series(model.feature_importances_, index=features_fit)
 features_importance.sort_values(ascending=False).head(15)
 
 # %%
+## PLOTS
 sklplot.metrics.plot_roc(Y_test, y_test_proba)
 sklplot.metrics.plot_lift_curve(Y_test, y_test_proba)
 sklplot.metrics.plot_cumulative_gain(Y_test, y_test_proba)
 sklplot.metrics.plot_ks_statistic(Y_test, y_test_proba)
+# %%
+
+## OOT PREDICT -------------------------------------------------------------------------------------
+y_proba_oot = model_pipeline.predict_proba(df_oot[features])
+
+sklplot.metrics.plot_roc(df_oot[target], y_proba_oot)
+sklplot.metrics.plot_lift_curve(df_oot[target], y_proba_oot)
+sklplot.metrics.plot_cumulative_gain(df_oot[target], y_proba_oot)
+sklplot.metrics.plot_ks_statistic(df_oot[target], y_proba_oot)
 # %%
